a864a1f0d3d53632a84dd7943db2f144e9d694e6
Frederik P <frepbh@gmail.com>
2022-12-21 20:18:21 +0100

    fixed small mistake

--- bdo_coupon_scanner/coupon_code.py
+++ bdo_coupon_scanner/coupon_code.py
@@ -5,8 +5,8 @@ class CouponCode:
     def __init__(self, code: str, date: date, origin_link: str):
         self.code = code
         self.date = date
-        self.article_link = origin_link
+        self.origin_link = origin_link
         pass
 
     def __str__(self) -> str:
-        return f"{self.code} [{self.date} | {self.article_link}]"
+        return f"{self.code} [{self.date} | {self.origin_link}]"
